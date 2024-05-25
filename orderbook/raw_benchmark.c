da0c3636e0c7ed2911cb0a930b7c2a2880349107
Marcus Lee <marcustutorial@hotmail.com>
2024-05-25 13:47:01 +0100

    feat: added more detailed benchmark

--- orderbook/raw_benchmark.c
+++ orderbook/raw_benchmark.c
@@ -58,17 +58,26 @@ struct state {
 //   printf(" in %ldns\n", elapsed_ns);
 // }
 
-uint64_t benchmark(struct state* state) {
+struct benchmark_result {
+  uint64_t market_elapsed_ns, market_count;
+  uint64_t limit_elapsed_ns, limit_count;
+  uint64_t cancel_elapsed_ns, cancel_count;
+  uint64_t amend_size_elapsed_ns, amend_size_count;
+};
+
+struct benchmark_result benchmark(struct state* state) {
   struct orderbook orderbook = orderbook_new();
   struct orderbook* ob = &orderbook;
   // struct event_handler handler = event_handler_new(handle_order_event);
   // orderbook_set_event_handler(ob, &handler);
 
-  struct timespec start;
-  clock_gettime(CLOCK_MONOTONIC, &start);
+  struct benchmark_result result = {};
 
   for (int i = 0; i < state->messages_len; i++) {
     const struct message message = state->messages[i];
+
+    struct timespec start;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
     switch (message.message_type) {
       case MESSAGE_TYPE_CREATED:
@@ -87,17 +96,37 @@ uint64_t benchmark(struct state* state) {
         orderbook_amend_size(ob, message.order_id, message.size);
         break;
     }
+
+    struct timespec end;
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    uint64_t elapsed_ns =
+        (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
+
+    switch (message.message_type) {
+      case MESSAGE_TYPE_CREATED:
+        if (message.price == 0) {
+          result.market_count++;
+          result.market_elapsed_ns += elapsed_ns;
+        } else {
+          result.limit_count++;
+          result.limit_elapsed_ns += elapsed_ns;
+        }
+        break;
+      case MESSAGE_TYPE_DELETED:
+        result.cancel_count++;
+        result.cancel_elapsed_ns += elapsed_ns;
+        break;
+      case MESSAGE_TYPE_CHANGED:
+        result.amend_size_count++;
+        result.amend_size_elapsed_ns += elapsed_ns;
+        break;
+    }
   }
-
-  struct timespec end;
-  clock_gettime(CLOCK_MONOTONIC, &end);
-
-  uint64_t elapsed_ns =
-      (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
 
   orderbook_free(&orderbook);
 
-  return elapsed_ns;
+  return result;
 }
 
 #define SAMPLE_SIZE 100
@@ -106,18 +135,44 @@ int main() {
   struct state state = {.messages_len = get_line_count(DATA),
                         .messages = parse_messages(DATA)};
 
-  uint64_t elapsed_ns = 0;
+  struct benchmark_result result;
+  uint64_t market_elapsed_ns = 0;
+  uint64_t limit_elapsed_ns = 0;
+  uint64_t cancel_elapsed_ns = 0;
+  uint64_t amend_size_elapsed_ns = 0;
 
   for (int i = 0; i < SAMPLE_SIZE; i++) {
-    elapsed_ns += benchmark(&state);
-    if (i != 0)
-      elapsed_ns /= 2;
+    result = benchmark(&state);
+    market_elapsed_ns += result.market_elapsed_ns;
+    limit_elapsed_ns += result.limit_elapsed_ns;
+    cancel_elapsed_ns += result.cancel_elapsed_ns;
+    amend_size_elapsed_ns += result.amend_size_elapsed_ns;
+    if (i != 0) {
+      market_elapsed_ns /= 2;
+      limit_elapsed_ns /= 2;
+      cancel_elapsed_ns /= 2;
+      amend_size_elapsed_ns /= 2;
+    }
   }
 
+  uint64_t total_elapsed_ns = market_elapsed_ns + limit_elapsed_ns +
+                              cancel_elapsed_ns + amend_size_elapsed_ns;
+
   printf("Over %d samples,\n", SAMPLE_SIZE);
-  printf("Took %.2fms to process %ld messages\n", elapsed_ns * 1e-6,
+  printf("Took %.2fms to process %ld messages\n", total_elapsed_ns * 1e-6,
          state.messages_len);
-  printf("Took %ldns to process 1 message\n", elapsed_ns / state.messages_len);
+  printf("Took %ldns to process 1 message\n",
+         total_elapsed_ns / state.messages_len);
+  printf("-------------------------------\n");
+  printf("orderbook_market: %ldns/op over %ld calls\n",
+         market_elapsed_ns / result.market_count, result.market_count);
+  printf("orderbook_limit: %ldns/op over %ld calls\n",
+         limit_elapsed_ns / result.limit_count, result.limit_count);
+  printf("orderbook_cancel: %ldns/op over %ld calls\n",
+         cancel_elapsed_ns / result.cancel_count, result.cancel_count);
+  printf("orderbook_amend_size: %ldns/op over %ld calls\n",
+         amend_size_elapsed_ns / result.amend_size_count,
+         result.amend_size_count);
 
   // Deallocate memory
   free(state.messages);
